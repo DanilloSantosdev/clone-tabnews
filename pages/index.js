557e61ebaefb1aa11c38cfb85da24a153857b32b
Danillo Pereira <danillopereira@outlook.com>
2026-03-18 00:23:29 +0000

    new modifies to surprise

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Image from 'next/image'
+
 function Home() {
-    return <h1>Ju, cinzin e Luci, eu amo vocês! Se me ama, passa aqui para me dar um beijo rs</h1>
+    return (
+        <>
+            <h1>Ju, cinzin e Luci, eu amo vocês! Se me ama, passa aqui para me dar um beijo rs</h1>
+            <Image
+              src="/assets/img/cats.jpeg"
+              alt="picture of my cats"
+              width={300}
+              height={500}
+            />
+        </>
+    )
 }
 
 export default Home;
