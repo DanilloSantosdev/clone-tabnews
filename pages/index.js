7fa913978d15eafab56bd0bbb4c40ab8227890ba
Danillo Pereira <danillopereira@outlook.com>
2026-03-28 21:23:49 +0000

    Adiciona scripts  and

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 function Home() {
-    return (
-        <>
-            <h1>Ju, cinzin e Luci, eu amo vocês! Se me ama, passa aqui para me dar um beijo rs</h1>
-            <Image
-              src="/assets/img/cats.jpeg"
-              alt="picture of my cats"
-              width={500}
-              height={500}
-            />
-            <Image
-              src="/assets/img/danju.jpeg"
-              alt="us forever"
-              width={500}
-              height={500}
-            />
-        </>
-    )
+  return (
+    <>
+      <h1>
+        Ju, cinzin e Luci, eu amo vocês! Se me ama, passa aqui para me dar um
+        beijo rs
+      </h1>
+      <Image
+        src="/assets/img/cats.jpeg"
+        alt="picture of my cats"
+        width={500}
+        height={500}
+      />
+      <Image
+        src="/assets/img/danju.jpeg"
+        alt="us forever"
+        width={500}
+        height={500}
+      />
+    </>
+  );
+}
+
+function teste1() {
+  console.log("teste1")
 }
 
 export default Home;
